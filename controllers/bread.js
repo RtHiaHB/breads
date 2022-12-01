3a16845706afce8649aa3058a0c24a2bafc52ce1
Thomas Davidson <tomd1969@gmail.com>
2022-12-01 18:01:50 -0500

    second commit

--- controllers/bread.js
+++ controllers/bread.js
@@ -2,7 +2,10 @@ const router = require('express').Router()
 const Bread = require('../models/bread')
 
 router.get('/', (req, res) => {
-    res.send(Bread)
+    res.render('index', {
+        breads: Bread
+    })
+
 })
 //query parameter
 router.get('/:index', (req, res) => {
