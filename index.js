dac40f982d8198c7087f700311299201182357b2
Thomas Davidson <tomd1969@gmail.com>
2022-12-01 19:07:38 -0500

    End 4.1

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const breadRoutes = require('./controllers/bread')
 const app = express();
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
+//Routes
 app.use('/breads', breadRoutes)
 
 const PORT = process.env.PORT || 8080;
