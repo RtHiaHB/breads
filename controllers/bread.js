f41d6029c65c2f384fb46c76ec554fd80a526496
Thomas Davidson <tomd1969@gmail.com>
2022-12-01 20:48:33 -0500

    Part 3

--- controllers/bread.js
+++ controllers/bread.js
@@ -7,6 +7,12 @@ router.get('/', (req, res) => {
     })
 
 })
+
+// Get:create new bread page
+router.get('/new', (req, res) => {
+    res.render('new')
+})
+
 //query parameter
 router.get('/:index', (req, res) => {
     const { index } = req.params;
@@ -14,6 +20,27 @@ router.get('/:index', (req, res) => {
         bread: Bread[index]
     })
     //res.send(Bread[index]);
+})
+
+router.post('/', (req, res) => {
+    const { hasGluten, image } = req.body
+    if (!image) req.body.image = 'https://suebeehomemaker.com/wp-content/uploads/2021/10/sliced-french-bread.jpg'
+    if (hasGluten === 'on') {
+        req.body.hasGluten = true;
+    } else {
+        req.body.hasGluten = false;
+    }
+    try {
+        new URL(image)
+    } catch (error) {
+        console.log('error saving image:', error)
+        res.render('new', {
+            error: 'not a valid image'
+        })
+        return
+    }
+    Bread.push(req.body)
+    res.redirect('/breads')
 })
 
 module.exports = router
--- views/index.jsx
+++ views/index.jsx
@@ -5,6 +5,11 @@ function Index ({ breads }) {
     return (
         <Default>
             <h2>Index Page</h2>
+            <div className='newButton'>
+                <a href='/breads/new'>
+                    <button>Add a new bread</button>
+                </a>
+            </div>
             <ul>
                 {
                     breads.map((bread, i) => {
